e9a358f25c95f989c6d0f0a48b25191ba44b0fb8
Maxsuel Aparecido Lima Santos <maxsuel.ap.l.santos@gmail.com>
2020-10-16 18:04:39 -0300

    Página principal

---
+++ site-exemplo/script.js
@@ -0,0 +1,13 @@
+let area = document.getElementById('area')
+
+function mover() {
+    area.style.background = 'blue'
+    area.style.color = 'black'
+    area.innerText = 'O mouse está aqui!'
+}
+
+function clicar() {
+    area.style.background = 'red'
+    area.style.color = 'green'
+    area.innerText = 'Você clicou!'
+}
